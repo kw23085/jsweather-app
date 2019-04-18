12196ef4eeb796a28e52f4b311345699e4167fcb
Kevin Wang <kw1017@gmail.com>
2019-04-17 22:12:04 -0700

    render api info onto frontend

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 window.addEventListener('load', () => {
     let long;
     let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
     
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
@@ -15,7 +18,14 @@ window.addEventListener('load', () => {
             })
             .then(data => {
                 console.log(data);
+                const { temperature, summary } = data.currently;
+                // Set DOM Elements from the API
+                temperatureDegree.textContent = temperature;
+                temperatureDescription.textContent = summary;
+                locationTimezone.textContent = data.timezone;
             })
         });
-    }       
+    }      
+    
+
 });
