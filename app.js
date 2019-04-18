58ffa0314efd1b119edd9619c767a1ebdd871f8e
Kevin Wang <kw1017@gmail.com>
2019-04-17 22:00:02 -0700

    made api work

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ window.addEventListener('load', () => {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
+
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const api = `${proxy}https://api.darksky.net/forecast/b28f1792375bab887da221f0a7dd813f/${lat},${long}`;
+            fetch(api)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+            })
         });
-    }
+    }       
 });
