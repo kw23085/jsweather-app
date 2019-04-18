3e046189a37ae142e0cdf638ef318ae2601213cb
Kevin Wang <kw1017@gmail.com>
2019-04-17 22:23:37 -0700

    add skycon

--- app.js
+++ app.js
@@ -18,14 +18,21 @@ window.addEventListener('load', () => {
             })
             .then(data => {
                 console.log(data);
-                const { temperature, summary } = data.currently;
+                const { temperature, summary, icon } = data.currently;
                 // Set DOM Elements from the API
                 temperatureDegree.textContent = temperature;
                 temperatureDescription.textContent = summary;
                 locationTimezone.textContent = data.timezone;
-            })
+                    //Set Icon
+                    setIcons(icon, document.querySelector('.icon'));
+            });
         });
     }      
     
-
+    function setIcons(icon, iconID) {
+        const skycons = new Skycons({color: "white"});
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+    }
 });
